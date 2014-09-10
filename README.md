94c4688f6ce307145a8a8ea7e1f72d3bb9c64083
Hondallica <hondallica@gmail.com>
2014-09-10 18:50:45 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+WebService::Openaura - It's new $module
+
+# SYNOPSIS
+
+    use WebService::Openaura;
+
+# DESCRIPTION
+
+WebService::Openaura is ...
+
+# LICENSE
+
+Copyright (C) Hondallica.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Hondallica <hondallica@gmail.com>
---
+++ lib/WebService/Openaura.pm
@@ -0,0 +1,39 @@
+package WebService::Openaura;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+WebService::Openaura - It's new $module
+
+=head1 SYNOPSIS
+
+    use WebService::Openaura;
+
+=head1 DESCRIPTION
+
+WebService::Openaura is ...
+
+=head1 LICENSE
+
+Copyright (C) Hondallica.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Hondallica E<lt>hondallica@gmail.comE<gt>
+
+=cut
+
